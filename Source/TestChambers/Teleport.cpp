ba28f7819b5294fc587b8dd9011bc52ad0e0f9ed
AtkinsJM <github@joshatkins.co.uk>
2020-01-09 23:01:59 +0000

    Created Teleport class and blueprint and set up teleporting.

---
+++ Source/TestChambers/Teleport.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teleport.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "PlayerCharacter.h"
+#include "Engine/World.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ATeleport::ATeleport()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
+	RootComponent = Root;
+	
+	ParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Particle System"));
+	ParticleSystem->SetupAttachment(Root);
+
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cylinder Mesh"));
+	Mesh->SetupAttachment(Root);
+
+	TeleportDelay = 1.0f;
+
+	ActorToTeleport = nullptr;
+
+	bIsTeleporting = false;
+}
+
+// Called when the game starts or when spawned
+void ATeleport::BeginPlay()
+{
+	Super::BeginPlay();
+	Root->OnComponentBeginOverlap.AddDynamic(this, &ATeleport::OnBeginOverlap);
+	Root->OnComponentEndOverlap.AddDynamic(this, &ATeleport::OnEndOverlap);
+}
+
+// Called every frame
+void ATeleport::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATeleport::OnBeginOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (bIsTeleporting) { return; }
+	bIsTeleporting = true;
+	ActorToTeleport = OtherActor;
+	GetWorld()->GetTimerManager().SetTimer(TeleportTimerHandle, this, &ATeleport::Teleport, TeleportDelay, false);
+}
+
+void ATeleport::OnEndOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+
+}
+
+void ATeleport::Teleport()
+{
+	ActorToTeleport->SetActorLocation(TeleportLocation.GetLocation());
+
+	if (TeleportCue)
+	{
+		UGameplayStatics::PlaySound2D(GetWorld(), TeleportCue);
+	}
+	ActorToTeleport = nullptr;
+	bIsTeleporting = false;
+}
---
+++ Source/TestChambers/Teleport.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Teleport.generated.h"
+
+UCLASS()
+class TESTCHAMBERS_API ATeleport : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATeleport();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UBoxComponent* Root;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UParticleSystemComponent* ParticleSystem;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
+	float TeleportDelay;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Properties")
+	FTransform TeleportLocation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sound")
+	class USoundCue* TeleportCue;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void Teleport();
+
+
+private:
+	AActor* ActorToTeleport;
+
+	FTimerHandle TeleportTimerHandle;
+
+	bool bIsTeleporting;
+};
